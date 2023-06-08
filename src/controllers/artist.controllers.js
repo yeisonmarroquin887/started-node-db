3089d176b17615294d54ae520b453f6bc962c89e
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-06-08 00:03:51 -0300

    first commit

---
+++ src/controllers/artist.controllers.js
@@ -0,0 +1,43 @@
+const catchError = require('../utils/catchError');
+const Artist = require('../models/Artist');
+
+const getAll = catchError(async(req, res) => {
+    const results = await Artist.findAll();
+    return res.json(results);
+});
+
+const create = catchError(async(req, res) => {
+    const result = await Artist.create(req.body);
+    return res.status(201).json(result);
+});
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await Artist.findByPk(id);
+    if(!result) return res.sendStatus(404);
+    return res.json(result);
+});
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    await Artist.destroy({ where: {id} });
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await Artist.update(
+        req.body,
+        { where: {id}, returning: true }
+    );
+    if(result[0] === 0) return res.sendStatus(404);
+    return res.json(result[1][0]);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const routerGenre = require('./genre.router');
+const routerArtist = require('./artist.router');
 const router = express.Router();
 
 // colocar las rutas aquí
+router.use('/genres', routerGenre)
+router.use('/artists', routerArtist)
 
 
 module.exports = router;
